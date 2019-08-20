5bbcf52a5a654021506f44d524469ed3b7e34e25
Diego Yasuhiko Kurisaki <diegoy@gmail.com>
2019-08-20 19:20:56 -0300

    Code fixes and add output

--- quiz/main.go
+++ quiz/main.go
@@ -1,35 +1,53 @@
 package main
 
 import (
-  "os"
-  "encoding/csv"
-  "fmt"
-  "flag"
+	"bufio"
+	"encoding/csv"
+	"flag"
+	"fmt"
+	"os"
+	"strings"
 )
 
 func main() {
-  fileName := flag.String("file", "problems.csv", "file name")
-  flag.Parse()
+	fileName := flag.String("file", "problems.csv", "file name")
+	flag.Parse()
 
-  file, err := os.Open(*fileName)
+	csvReader := openCsv(*fileName)
+	inputReader := bufio.NewReader(os.Stdin)
 
-  if err != nil {
-    println("Oops alguma coisa deu errada.")
-    println(err.Error())
-  } else {
-    println("File is opened!")
-    csvReader := csv.NewReader(file)
+	rightAnswers, total := 0, 0
 
-    for {
-      line, err := csvReader.Read()
-      if err != nil && err.Error() == "EOF" {
-        println("End of file bye!")
-        break
-      }
-      fmt.Printf("Question: %s, Answer: %s\n", line[0], line[1])
-    }
-  }
+	for {
+		line, err := csvReader.Read()
+		if err != nil && err.Error() == "EOF" {
+			fmt.Printf("-----------------------\nFinished!\n-----------------------\n")
+			fmt.Printf("Total questions: %d\n", total)
+			fmt.Printf("Correct answers: %d\n", rightAnswers)
+			fmt.Printf("Accuracy: %.2f%%\n", float64(rightAnswers)/float64(total)*100)
+			fmt.Printf("-----------------------\n")
+			break
+		}
+		question, answer := line[0], line[1]
+		fmt.Printf("%s = ", question)
+
+		userAnswer, _ := inputReader.ReadString('\n')
+		userAnswer = strings.TrimSuffix(userAnswer, "\n")
+
+		if strings.Compare(answer, userAnswer) == 0 {
+			rightAnswers++
+		}
+		total++
+	}
 }
 
-func filename() {
+func openCsv(fileName string) *csv.Reader {
+	file, err := os.Open(fileName)
+
+	if err != nil {
+		println("Oops alguma coisa deu errada.")
+		panic(err.Error())
+	}
+
+	return csv.NewReader(file)
 }
